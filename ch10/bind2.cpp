189086dd0d6ad0c56747e9c13d6e6427dca504a3
Yang Ge <eroicaleo@gmail.com>
2016-01-19 14:06:57 -0800

    updates code for chapter 10.2.2 bind2.cpp
      bind to shared_ptr and member function which
      modifies the object itself.

--- ch10/bind2.cpp
+++ ch10/bind2.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <memory>
 
 using namespace std;
 using namespace std::placeholders;
@@ -20,6 +21,9 @@ class Person {
         void print2(const string &prefix) const {
             cout << prefix << name << endl;
         }
+        void setName(const string &n) {
+            name = n;
+        }
 };
 
 int main() {
@@ -35,4 +39,19 @@ int main() {
     cout << endl;
 
     bind(&Person::print2, _1, "This is: ")(Person("nico"));
+
+    shared_ptr<Person> p1{new Person{"Yang"}};
+    shared_ptr<Person> p2{new Person{"Yukan"}};
+
+    vector<shared_ptr<Person>> sp;
+    sp.push_back(p1);
+    sp.push_back(p2);
+
+    for_each(sp.begin(), sp.end(), bind(&Person::print, _1));
+
+    for_each(coll.begin(), coll.end(), bind(&Person::setName, _1, "Paul"));
+    for_each(coll.begin(), coll.end(),
+            bind(&Person::print, _1));
+    cout << endl;
+
 }
