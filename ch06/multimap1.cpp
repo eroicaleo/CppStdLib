ba0ed358ec0e1e1de951398886403345188799dd
Yang Ge <eroicaleo@gmail.com>
2016-02-05 01:05:55 -0800

    Added code for multiset1 and multimap1

---
+++ ch06/multimap1.cpp
@@ -0,0 +1,25 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    multimap<int, string> coll;
+
+    coll = {
+        {5,"tagged"},
+        {2,"a"},
+        {1,"this"},
+        {4,"of"},
+        {6,"strings"},
+        {1,"is"},
+        {3,"multimap"}
+    };
+
+    for (const auto& elem: coll) {
+        cout << elem.second << " ";
+    }
+    cout << endl;
+
+}
---
+++ ch06/multiset1.cpp
@@ -0,0 +1,23 @@
+#include <set>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    multiset<string> cities {"Braunschweig", "Hanover", "Frankfurt", "New York",
+    "Chicago", "Toronto", "Paris", "Frankfurt"};
+
+    for (const auto& elem: cities) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    cities.insert({"London", "Munich", "Hanover", "Braunschweig"});
+
+    for (const auto& elem: cities) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+}
